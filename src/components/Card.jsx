f26866f10ced1e11d3bd2d4c9ed760725a85c24a
kirk <kirk.betzina@gmail.com>
2021-05-01 13:44:57 -0400

    rendered three cards in main with .map

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Card = (props) => {
-   
+    const numArr = [1,2,3]
     return (
-        <div>
-            <h1>This is a card component</h1>
-            <img src="" alt=""/>
-            <button>LINK ME</button>
+        <div className ='Cards'>
+            {
+                numArr.map((number, index) =>(
+                    <div key={index}>
+                        <h1>This is a card component</h1>
+                        <img src="" alt=""/>
+                        <button>LINK ME</button>
+                    </div>
+                ))
+            }
         </div>
     )  
 }
